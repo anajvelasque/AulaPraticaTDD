0a40df8c17ac86dceb123151756029e47be90515
Ana Julia <anajvelasque@gmail.com>
2022-09-26 16:40:02 -0300

    Commit 4

--- Money.java
+++ Money.java
@@ -7,8 +7,13 @@ abstract class Money{
         Money money = (Money) object;
         return amount == money.amount && getClass().equals(money.getClass());
     }
-    
+
     static Dollar dollar(int amount)  {
         return new Dollar(amount);
+    }
+
+    static Money franc(int amount)
+    {
+        return new Franc(amount);
     }
 }
